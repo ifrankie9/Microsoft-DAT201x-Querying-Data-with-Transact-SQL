819be7e09a97ac36fb3564e8baf4c42dc7459755
Frankie_Wang <wangqiuchen0917@gmail.com>
2018-04-30 10:45:53 -0400

    Update Module8-Exercises
    Re-structure

--- Exercises/Module8-Exercises.sql
+++ Exercises/Module8-Exercises.sql
@@ -1,92 +1,58 @@
 SELECT a.CountryRegion, a.StateProvince, SUM(soh.TotalDue) AS Revenue
-F
-ROM SalesLT.Address AS a
-
+FROM SalesLT.Address AS a
 JOIN SalesLT.CustomerAddress AS ca
-
 ON a.AddressID = ca.AddressID
 JOIN SalesLT.Customer AS c
-
 ON ca.CustomerID = c.CustomerID
-
 JOIN SalesLT.SalesOrderHeader as soh
-
 ON c.CustomerID = soh.CustomerID
-
 -- Modify GROUP BY to use ROLLUP
-
 GROUP BY ROLLUP (a.CountryRegion, a.StateProvince)
-
 ORDER BY a.CountryRegion, a.StateProvince;
+
+
 
 SELECT a.CountryRegion, a.StateProvince,
-
-IIF(GROUPING_ID(CountryRegion) = 1 AND GROUPING_ID(a.StateProvince) = 1, 'Total', IIF(GROUPING_ID(StateProvince) = 1, a.CountryRegion + ' Subtotal', a.StateProvince + ' Subtotal')) AS Level,
-
+IIF(GROUPING_ID(CountryRegion) = 1 AND GROUPING_ID(a.StateProvince) = 1, 'Total', IIF(GROUPING_ID(StateProvince) = 1, a.CountryRegion + ' Subtotal', a.StateProvince + ' Subtotal')) AS Level, 
 SUM(soh.TotalDue) AS Revenue
 FROM SalesLT.Address AS a
-
 JOIN SalesLT.CustomerAddress AS ca
-
 ON a.AddressID = ca.AddressID
-
 JOIN SalesLT.Customer AS c
-
 ON ca.CustomerID = c.CustomerID
-
 JOIN SalesLT.SalesOrderHeader as soh
-
 ON c.CustomerID = soh.CustomerID
-
 GROUP BY ROLLUP(a.CountryRegion, a.StateProvince)
-
 ORDER BY a.CountryRegion, a.StateProvince;
 
+
+
 SELECT a.CountryRegion, a.StateProvince, a.City,
-
 CHOOSE (1 + GROUPING_ID(CountryRegion) + GROUPING_ID(StateProvince) + GROUPING_ID(City),
-        
-a.City + ' Subtotal', a.StateProvince + ' Subtotal',
-        
-a.CountryRegion + ' Subtotal', 'Total') AS Level,
-
+        a.City + ' Subtotal', a.StateProvince + ' Subtotal',
+        a.CountryRegion + ' Subtotal', 'Total') AS Level,
 SUM(soh.TotalDue) AS Revenue
-
 FROM SalesLT.Address AS a
-
 JOIN SalesLT.CustomerAddress AS ca
-
 ON a.AddressID = ca.AddressID
-
 JOIN SalesLT.Customer AS c
-
 ON ca.CustomerID = c.CustomerID
-
 JOIN SalesLT.SalesOrderHeader as soh
-
 ON c.CustomerID = soh.CustomerID
-
 GROUP BY ROLLUP(a.CountryRegion, a.StateProvince, a.City)
-
 ORDER BY a.CountryRegion, a.StateProvince, a.City;
 
+
+
 SELECT * FROM
-
 (SELECT cat.ParentProductCategoryName, CompanyName, LineTotal
- 
-FROM SalesLT.SalesOrderDetail AS sod
- 
-JOIN SalesLT.SalesOrderHeader AS soh ON sod.SalesOrderID = soh.SalesOrderID
- 
-JOIN SalesLT.Customer AS cust ON soh.CustomerID = cust.CustomerID
- 
-JOIN SalesLT.Product AS prod ON sod.ProductID = prod.ProductID
- 
-JOIN SalesLT.vGetAllCategories AS cat 
-ON prod.ProductcategoryID = cat.ProductCategoryID) AS catsales
-
+ FROM SalesLT.SalesOrderDetail AS sod
+ JOIN SalesLT.SalesOrderHeader AS soh ON sod.SalesOrderID = soh.SalesOrderID
+ JOIN SalesLT.Customer AS cust ON soh.CustomerID = cust.CustomerID
+ JOIN SalesLT.Product AS prod ON sod.ProductID = prod.ProductID
+ JOIN SalesLT.vGetAllCategories AS cat 
+ ON prod.ProductcategoryID = cat.ProductCategoryID) AS catsales
 PIVOT (SUM(LineTotal)FOR ParentProductCategoryName
-
 IN ([Accessories], [Bikes], [Clothing], [Components])) AS pivotedsales
-
 ORDER BY CompanyName;
+
