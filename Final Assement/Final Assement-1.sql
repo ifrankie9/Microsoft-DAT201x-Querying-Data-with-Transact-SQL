f6aa7c57cecff5ce5182df469621a5f2e3f82875
Frankie_Wang <wangqiuchen0917@gmail.com>
2018-04-30 10:54:35 -0400

    Update Final Assessment -1

--- Final Assement/Final Assement-1.sql
+++ Final Assement/Final Assement-1.sql
@@ -5,7 +5,6 @@ SELECT DISTINCT CategoryID FROM Dbo.Products
 
 
 SELECT ProductName FROM Dbo.Products
-
 WHERE UnitsInStock > 20;
 
 
@@ -15,33 +14,24 @@ ORDER BY UnitPrice DESC;
 
 
 SELECT ProductID, ProductName, QuantityPerUnit FROM Dbo.Products
-
 ORDER BY ProductName;
 
 
 SELECT ProductID, ProductName, UnitPrice FROM Dbo.Products
-
 ORDER BY UnitsInStock DESC
-
 OFFSET 10 ROWS FETCH FIRST 5 ROW ONLY
 
 
 SELECT FirstName + 
-
 ' has an EmployeeID of ' + 
-
 CAST(EmployeeID AS VarChar(5)) + 
-
-' and was born '
-+ 
+' and was born ' + 
 CONVERT(NVARCHAR(30), BirthDate, 126) AS Employees 
-
 FROM Employees
 
 
 SELECT ShipName + ' is from ' + 
 COALESCE(ShipCity, ShipRegion, ShipCountry) AS Destination
-
 FROM Dbo.Orders
 
 
@@ -49,15 +39,9 @@ SELECT ShipName, ISNULL(ShipPostalCode, 'unknown') FROM Dbo.Orders
 
 
 SELECT CompanyName,
-
 CASE
-   
-WHEN Fax IS NULL THEN 'modern'
-   
-ELSE 'outdated'
-
+   WHEN Fax IS NULL THEN 'modern'
+   ELSE 'outdated'
 END AS FaxStatus
-
 FROM Dbo.Suppliers
-
 
