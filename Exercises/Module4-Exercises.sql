6fff832c0f3d2745d4e75e8fa27e523c54db263c
Frankie_Wang <wangqiuchen0917@gmail.com>
2018-04-30 10:40:03 -0400

    Update Module4-Exercises
    Re-structure

--- Exercises/Module4-Exercises.sql
+++ Exercises/Module4-Exercises.sql
@@ -1,165 +1,97 @@
 -- select the CompanyName, AddressLine1 columns
-
 -- alias as per the instructions
-
 SELECT c.CompanyName, a.AddressLine1, a.City, 'Billing' AS AddressType
-
 FROM SalesLT.Customer AS c
-
 JOIN SalesLT.CustomerAddress AS ca
-
 -- join based on CustomerID
-
 ON c.CustomerID = ca.CustomerID
-
 -- join another table
-
 JOIN SalesLT.Address AS a
-
 -- join based on AddressID
-
 ON ca.AddressID = a.AddressID
-
 -- filter for where the correct AddressType
-
 WHERE ca.AddressType = 'Main Office';
 
+
 -- edit this
-
 SELECT c.CompanyName, a.AddressLine1, a.City, 'Shipping' AS AddressType
-
 FROM SalesLT.Customer AS c
-J
-OIN SalesLT.CustomerAddress AS ca
-
+JOIN SalesLT.CustomerAddress AS ca
 ON c.CustomerID = ca.CustomerID
-
 JOIN SalesLT.Address AS a
-
 ON ca.AddressID = a.AddressID
--
-- edit this
-
+-- edit this
 WHERE ca.AddressType = 'Shipping';
 
+
+
 SELECT c.CompanyName, a.AddressLine1, a.City, 'Billing' AS AddressType
-
 FROM SalesLT.Customer AS c
-
 JOIN SalesLT.CustomerAddress AS ca
-
 ON c.CustomerID = ca.CustomerID
-
 JOIN SalesLT.Address AS a
-
 ON ca.AddressID = a.AddressID
-
 WHERE ca.AddressType = 'Main Office'
 
+
+
 -- edit this as per the instructions
-
 UNION ALL
-
 SELECT c.CompanyName, a.AddressLine1, a.City, 'Shipping' AS AddressType
-
 FROM SalesLT.Customer AS c
-
 JOIN SalesLT.CustomerAddress AS ca
-
 ON c.CustomerID = ca.CustomerID
-
 JOIN SalesLT.Address AS a
-
 ON ca.AddressID = a.AddressID
-
 WHERE ca.AddressType = 'Shipping'
-
 ORDER BY c.CompanyName, AddressType;
 
 SELECT c.CompanyName
-
 FROM SalesLT.Customer AS c
-
 JOIN SalesLT.CustomerAddress AS ca
-
 ON c.CustomerID = ca.CustomerID
-
 JOIN SalesLT.Address AS a
-
 ON ca.AddressID = a.AddressID
-
 WHERE ca.AddressType = 'Main Office'
-
 EXCEPT
 
 SELECT c.CompanyName
-
 FROM SalesLT.Customer AS c
-
 JOIN SalesLT.CustomerAddress AS ca
-
 ON c.CustomerID = ca.CustomerID
-
 JOIN SalesLT.Address AS a
-
 ON ca.AddressID = a.AddressID
-
 -- filter for the appropriate AddressType
-
 WHERE ca.AddressType = 'Shipping'
-
 ORDER BY c.CompanyName;
 
+
+
 -- select the CompanyName column
-
 SELECT c.CompanyName
-
 -- from the appropriate table
-
 FROM SalesLT.Customer AS c
-
 -- use the appropriate join with the appropriate table
-
 JOIN SalesLT.CustomerAddress AS ca
-
 -- join based on CustomerID
-
 ON c.CustomerID = ca.CustomerID
-
 -- use the appropriate join with the appropriate table
-
 JOIN SalesLT.Address AS a
-
 -- join based on AddressID
-
 ON a.AddressID = ca.AddressID
-
 -- filter based on AddressType
-
 WHERE ca.AddressType = 'Main Office'
-
 INTERSECT
-
 -- select the CompanyName column
-
 SELECT c.CompanyName
 FROM SalesLT.Customer AS c
-
 -- use the appropriate join with the appropriate table
-
 JOIN SalesLT.CustomerAddress AS ca
-
 ON c.CustomerID = ca.CustomerID
-
 JOIN SalesLT.Address AS a
-
 -- join based on AddressID
-
 ON a.AddressID = ca.AddressID
-
 -- filter based on AddressType
-
 WHERE ca.AddressType = 'Shipping'
-
 ORDER BY c.CompanyName;
 
