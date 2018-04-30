5affa8e39600a3efc733ebe85435a84b8e01ba6d
Frankie_Wang <wangqiuchen0917@gmail.com>
2018-04-30 10:41:36 -0400

    Update Module5-Exercises
    Re-structure

--- Exercises/Module5-Exercises.sql
+++ Exercises/Module5-Exercises.sql
@@ -1,112 +1,73 @@
 -- select ProductID and use the appropriate functions with the appropriate columns
 SELECT ProductId, UPPER(Name) AS ProductName, ROUND(Weight, 0) AS ApproxWeight
-
 FROM SalesLT.Product;
 
-SELECT ProductID, UPPER(Name) AS ProductName, ROUND(Weight, 0) AS ApproxWeight,
-      
 
+SELECT ProductID, UPPER(Name) AS ProductName, ROUND(Weight, 0) AS ApproxWeight,
 -- get the year of the SellStartDate
-       
 YEAR(SellStartDate) as SellStartYear,
-       
 -- get the month datepart of the SellStartDate
-       
 DATENAME(m, SellStartDate) as SellStartMonth
-
 FROM SalesLT.Product;
 
-SELECT ProductID, UPPER(Name) AS ProductName, ROUND(Weight, 0) AS ApproxWeight,
-      
 
-YEAR(SellStartDate) as SellStartYear,
-       
-DATENAME(m, SellStartDate) as SellStartMonth,
-       
--- use the appropriate function to extract substring from ProductNumber
-      
+SELECT ProductID, UPPER(Name) AS ProductName, ROUND(Weight, 0) AS ApproxWeight,
+YEAR(SellStartDate) as SellStartYear,    
+DATENAME(m, SellStartDate) as SellStartMonth,   
+-- use the appropriate function to extract substring from ProductNumber  
 SUBSTRING(ProductNumber, 0, 3) AS ProductType
-
 FROM SalesLT.Product;
 
-SELECT ProductID, UPPER(Name) AS ProductName, ROUND(Weight, 0) AS ApproxWeight,
-      
-YEAR(SellStartDate) as SellStartYear,
-       
-DATENAME(m, SellStartDate) as SellStartMonth,
-       
-LEFT(ProductNumber, 2) AS ProductType
 
+SELECT ProductID, UPPER(Name) AS ProductName, ROUND(Weight, 0) AS ApproxWeight,
+YEAR(SellStartDate) as SellStartYear,     
+DATENAME(m, SellStartDate) as SellStartMonth,       
+LEFT(ProductNumber, 2) AS ProductType
 FROM SalesLT.Product
 -- filter for numeric product size data
-
 WHERE ISNUMERIC(Size) = 1;
 
+
 -- select CompanyName and TotalDue columns
-
-SELECT CompanyName, TotalDue AS Revenue,
-       
--- get ranking and order by appropriate column
-       
+SELECT CompanyName, TotalDue AS Revenue,       
+-- get ranking and order by appropriate column       
 RANK() OVER (ORDER BY TotalDue DESC) AS RankByRevenue
-
 FROM SalesLT.SalesOrderHeader AS SOH
-
 -- use appropriate join on appropriate table
 JOIN SalesLT.Customer AS C
-
 ON SOH.CustomerID = C.CustomerID;
 
+
 -- select the Name column and use the appropriate function with the appropriate column
-
 SELECT Name, SUM(LineTotal) AS TotalRevenue
-
 FROM SalesLT.SalesOrderDetail AS SOD
-
 -- use the appropriate join
-
 JOIN SalesLT.Product AS P
-
 -- join based on ProductID
-
 ON SOD.ProductID = P.ProductID
-
 GROUP BY P.Name
 ORDER BY SUM(LineTotal) DESC;
 
+
 SELECT Name, SUM(LineTotal) AS TotalRevenue
-
 FROM SalesLT.SalesOrderDetail AS SOD
-
 JOIN SalesLT.Product AS P
-
 ON SOD.ProductID = P.ProductID
-
 -- filter as per the instructions
-
 WHERE P.ListPrice > 1000
-
 GROUP BY P.Name
 ORDER BY TotalRevenue DESC;
+
+
 
 SELECT Name, SUM(LineTotal) AS TotalRevenue
-
 FROM SalesLT.SalesOrderDetail AS SOD
-
 JOIN SalesLT.Product AS P
-
 ON SOD.ProductID = P.ProductID
-
 WHERE P.ListPrice > 1000
-
 GROUP BY P.Name
-
 -- add having clause as per instructions
-
 HAVING SUM(LineTotal) > 20000
-
 ORDER BY TotalRevenue DESC;
-
-
 
 
