e74630e1eea6cc9fe6facb7ed5d9310d28bcdbfb
Frankie_Wang <wangqiuchen0917@gmail.com>
2018-04-30 10:38:07 -0400

    Update Module3-Exercises
    Re-structure

--- Exercises/Module3-Exercises.sql
+++ Exercises/Module3-Exercises.sql
@@ -1,88 +1,55 @@
 -- select the CompanyName, SalesOrderId, and TotalDue columns from the appropriate tables
-
 SELECT c.CompanyName, oh.SalesOrderId, oh.TotalDue
-
 FROM SalesLT.Customer AS c
 JOIN SalesLT.SalesOrderHeader AS oh
-
 -- join tables based on CustomerID
-
 ON c.CustomerID = oh.CustomerID;
+
 
 SELECT c.CompanyName, a.AddressLine1, ISNULL(a.AddressLine2, '') AS AddressLine2, a.City, a.StateProvince, a.PostalCode, a.CountryRegion, oh.SalesOrderID, oh.TotalDue
 FROM SalesLT.Customer AS c
-
 -- join the SalesOrderHeader table
-
 JOIN SalesLT.SalesOrderHeader AS oh
 ON oh.CustomerID = c.CustomerID
-
 -- join the CustomerAddress table
-
 JOIN SalesLT.CustomerAddress AS ca
-
 -- filter for where the AddressType is 'Main Office'
-
 ON c.CustomerID = ca.CustomerID AND AddressType = 'Main Office'
-
 JOIN SalesLT.Address AS a
-
 ON ca.AddressID = a.AddressID;
 
+
 -- select the CompanyName, FirstName, LastName, SalesOrderID and TotalDue columns
-
 -- from the appropriate tables
-
 SELECT c.CompanyName, c.FirstName, c.LastName, oh.SalesOrderID, oh.TotalDue
-
 FROM SalesLT.Customer AS c
-
 LEFT JOIN SalesLT.SalesOrderHeader AS oh
-
 -- join based on CustomerID
-
 ON c.CustomerID = oh.CustomerID
-
 -- order the SalesOrderIDs from highest to lowest
-
 ORDER BY oh.SalesOrderID DESC;
 
+
 SELECT c.CompanyName, c.FirstName, c.LastName, c.Phone
-
 FROM SalesLT.Customer AS c
-
 LEFT JOIN SalesLT.CustomerAddress AS ca
-
 -- join based on CustomerID
-
 ON c.CustomerID = ca.CustomerID
-
 -- filter for when the AddressID doesn't exist
-
 WHERE ca.AddressID IS NULL;
 
+
 SELECT c.CustomerID, p.ProductID
-
 FROM SalesLT.Customer AS c
-
 FULL JOIN SalesLT.SalesOrderHeader AS oh
 ON c.CustomerID = oh.CustomerID
-
 FULL JOIN SalesLT.SalesOrderDetail AS od
-
 -- join based on the SalesOrderID
-
 ON od.SalesOrderID = oh.SalesOrderID
-
 FULL JOIN SalesLT.Product AS p
-
 -- join based on the ProductID
-
 ON p.ProductID = od.ProductID
-
 -- filter for nonexistent SalesOrderIDs
-
 WHERE oh.SalesOrderID IS NULL
-
 ORDER BY ProductID, CustomerID;
 
